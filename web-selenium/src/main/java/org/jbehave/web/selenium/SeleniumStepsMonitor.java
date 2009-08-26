c8f60f71fe76dfe1512e5a4453e30f09565dd03c
mauro <mauro@df8dfae0-ecdd-0310-b8d8-a050f7ac8317>
2009-08-26 20:37:00 +0000

    Added SeleniumStepsMonitor
    git-svn-id: https://svn.codehaus.org/jbehave/trunk/web@1202 df8dfae0-ecdd-0310-b8d8-a050f7ac8317

---
+++ web-selenium/src/main/java/org/jbehave/web/selenium/SeleniumStepsMonitor.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package org.jbehave.web.selenium;
+
+import java.lang.reflect.Type;
+
+import org.jbehave.scenario.steps.StepMonitor;
+
+import com.thoughtworks.selenium.Selenium;
+
+class SeleniumStepsMonitor implements StepMonitor {
+	private final Selenium selenium;
+	private final SeleniumContext seleniumContext;
+	private final StepMonitor delegate;
+
+	SeleniumStepsMonitor(Selenium selenium,
+			SeleniumContext seleniumContext, StepMonitor delegate) {
+		this.selenium = selenium;
+		this.seleniumContext = seleniumContext;
+		this.delegate = delegate;
+	}
+
+	public void performing(String step){
+		String context = seleniumContext.getCurrentScenario() + "<br>" + step;
+		selenium.setContext(context);
+		delegate.performing(step);
+	}
+
+	public void convertedValueOfType(String value, Type type, Object converted,
+			Class<?> converterClass) {
+		delegate.convertedValueOfType(value, type, converted, converterClass);
+	}
+
+	public void stepMatchesPattern(String step, boolean matches, String pattern) {
+		delegate.stepMatchesPattern(step, matches, pattern);
+	}
+
+}
