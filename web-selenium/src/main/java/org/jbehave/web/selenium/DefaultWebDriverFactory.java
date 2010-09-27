4f3442f376fa432ef69e857182de5043a80346b5
Paul Hammant <paul@hammant.org>
2010-09-27 07:56:51 -0500

    JBEHAVE-345: make factory more flexible than just Firefox

--- web-selenium/src/main/java/org/jbehave/web/selenium/DefaultWebDriverFactory.java
+++ web-selenium/src/main/java/org/jbehave/web/selenium/DefaultWebDriverFactory.java
@@ -1,13 +1,38 @@
 package org.jbehave.web.selenium;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 /**
- * Default factory that provides instances of {@link FirefoxDriver}s.
+ * Makes WebDriver instances as needed.
+ * These can be per-story, per-scenario, or one for all stories.
  */
 public class DefaultWebDriverFactory extends DelegatingWebDriverFactory {
 
+    private Class<? extends WebDriver> driver;
+
+    /**
+     * Provides instances of a supplied type
+     * @param driver the WebDriver type to instantiate.
+     */
+    public DefaultWebDriverFactory(Class<? extends WebDriver> driver) {
+        this.driver = driver;
+    }
+
+    /**
+     * Provides instances of {@link FirefoxDriver}s.
+     */
+    public DefaultWebDriverFactory() {
+        this.driver = FirefoxDriver.class;
+    }
+
     public void initialize() {
-        delegate = new FirefoxDriver();
+        try {
+            delegate = driver.newInstance();
+        } catch (InstantiationException e) {
+            new UnsupportedOperationException(e);
+        } catch (IllegalAccessException e) {
+            new UnsupportedOperationException(e);
+        }
     }
 }
