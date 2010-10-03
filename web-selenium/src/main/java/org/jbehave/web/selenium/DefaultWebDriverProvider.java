868f01e97ed425268c558aba75c1eef9bd6c34c4
Mauro Talevi <mauro.talevi@aquilonia.org>
2010-10-03 23:27:28 +0100

    JBEHAVE-345: Pulled up TypeWebDriverProvider from default impl.

--- web-selenium/src/main/java/org/jbehave/web/selenium/DefaultWebDriverProvider.java
+++ web-selenium/src/main/java/org/jbehave/web/selenium/DefaultWebDriverProvider.java
@@ -1,40 +1,17 @@
 package org.jbehave.web.selenium;
 
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 /**
- * Provides WebDriver instances of given type, instantiating it using the
- * default constructor.
+ * Provides FirefoxDriver instances.
  */
-public class DefaultWebDriverProvider extends DelegatingWebDriverProvider {
-
-    private Class<? extends WebDriver> type;
+public class DefaultWebDriverProvider extends TypeWebDriverProvider {
 
     /**
      * Provides instances of {@link FirefoxDriver}s.
      */
     public DefaultWebDriverProvider() {
-        this(FirefoxDriver.class);
-    }
-
-    /**
-     * Provides instances of a supplied type
-     * 
-     * @param type the WebDriver type to instantiate.
-     */
-    public DefaultWebDriverProvider(Class<? extends WebDriver> type) {
-        this.type = type;
-    }
-
-    public void initialize() {
-        try {
-            delegate = type.newInstance();
-        } catch (InstantiationException e) {
-            new UnsupportedOperationException(e);
-        } catch (IllegalAccessException e) {
-            new UnsupportedOperationException(e);
-        }
+        super(FirefoxDriver.class);
     }
 
 }
