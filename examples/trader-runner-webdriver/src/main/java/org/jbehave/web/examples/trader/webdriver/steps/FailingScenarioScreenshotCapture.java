d2f4cff278b8ac99dc998a52142a990c099cb9c3
Paul Hammant <paul@hammant.org>
2010-11-05 07:12:49 -0500

    glitch in exampls fixed

--- examples/trader-runner-webdriver/src/main/java/org/jbehave/web/examples/trader/webdriver/steps/FailingScenarioScreenshotCapture.java
+++ examples/trader-runner-webdriver/src/main/java/org/jbehave/web/examples/trader/webdriver/steps/FailingScenarioScreenshotCapture.java
@@ -2,10 +2,10 @@ package org.jbehave.web.examples.trader.webdriver.steps;
 
 import org.jbehave.core.annotations.AfterScenario;
 import org.jbehave.core.annotations.AfterScenario.Outcome;
-import org.jbehave.web.selenium.PerScenarioWebDriverSteps;
 import org.jbehave.web.selenium.WebDriverProvider;
+import org.jbehave.web.selenium.WebDriverSteps;
 
-public class FailingScenarioScreenshotCapture extends PerScenarioWebDriverSteps {
+public class FailingScenarioScreenshotCapture extends WebDriverSteps {
 
     public FailingScenarioScreenshotCapture(WebDriverProvider driverProvider) {
         super(driverProvider);
@@ -15,13 +15,10 @@ public class FailingScenarioScreenshotCapture extends PerScenarioWebDriverSteps 
     public void afterScenarioFailure() throws Exception {
         String screenshotPath = "target/screenshots/failed-scenario-" + System.currentTimeMillis() + ".png";
         driverProvider.saveScreenshotTo(screenshotPath);        
-        super.afterScenario();
     }
 
-    @Override
     @AfterScenario(uponOutcome = Outcome.SUCCESS)
     public void afterScenario() throws Exception {
-        super.afterScenario();
     }
 
 
