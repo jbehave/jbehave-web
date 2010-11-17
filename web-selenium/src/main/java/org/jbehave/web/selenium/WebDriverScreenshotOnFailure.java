e87b45cf9bd323353aecd21ecea73651b95be5bb
Paul Hammant <paul@hammant.org>
2010-11-17 17:38:10 -0600

    message as to where the screen shot went

--- web-selenium/src/main/java/org/jbehave/web/selenium/WebDriverScreenshotOnFailure.java
+++ web-selenium/src/main/java/org/jbehave/web/selenium/WebDriverScreenshotOnFailure.java
@@ -4,7 +4,8 @@ import org.jbehave.core.annotations.AfterScenario;
 import org.jbehave.core.annotations.AfterScenario.Outcome;
 
 /**
- * WebDriverSteps that save screenshot upon failure in a scenario outcome.
+ * WebDriverSteps that save screen-shot upon failure in a scenario outcome.
+ * Not all WebDriver implement screen-shot capability
  */
 public class WebDriverScreenshotOnFailure extends WebDriverSteps {
 
@@ -15,7 +16,8 @@ public class WebDriverScreenshotOnFailure extends WebDriverSteps {
     @AfterScenario(uponOutcome = Outcome.FAILURE)
     public void afterScenarioFailure() throws Exception {
         String screenshotPath = "target/screenshots/failed-scenario-" + System.currentTimeMillis() + ".png";
-        driverProvider.saveScreenshotTo(screenshotPath);        
+        driverProvider.saveScreenshotTo(screenshotPath);
+        System.out.println("Screenshot may or may not have been saved to '" + screenshotPath +"' depending on whether the WebDriver implementation supports screen shots");
     }
 
 }
