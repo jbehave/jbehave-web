6aefd45598435b5859a4276b57df23f01f17e5fa
Paul Hammant <paul@hammant.org>
2011-01-30 07:42:04 -0600

    refactoring to allow the users to modify output directory for screenshots

--- web-selenium/src/main/java/org/jbehave/web/selenium/WebDriverScreenshotOnFailure.java
+++ web-selenium/src/main/java/org/jbehave/web/selenium/WebDriverScreenshotOnFailure.java
@@ -6,7 +6,7 @@ import org.jbehave.core.failures.UUIDExceptionWrapper;
 
 /**
  * WebDriverSteps that save screen-shot upon failure in a scenario outcome.
- * Not all WebDriver implement screen-shot capability
+ * Not all WebDriver implementations screen-shot capability
  */
 public class WebDriverScreenshotOnFailure extends WebDriverSteps {
 
@@ -17,12 +17,21 @@ public class WebDriverScreenshotOnFailure extends WebDriverSteps {
     @AfterScenario(uponOutcome = Outcome.FAILURE)
     public void afterScenarioFailure(UUIDExceptionWrapper uuidWrappedFailure) throws Exception {
 
-        String screenshotPath = "target/jbehave/screenshots/failed-scenario-" + uuidWrappedFailure.getUUID() + ".png";
-        if ( driverProvider.saveScreenshotTo(screenshotPath) ){
+        String screenshotPath = getScreenShotRootPath() + "/failed-scenario-" + uuidWrappedFailure.getUUID() + ".png";
+        if ( driverProvider.saveScreenshotTo(screenshotPath) ) {
             System.out.println("Screenshot has been saved to '" + screenshotPath +"'");
         } else {
             System.out.println(driverProvider.get().getClass().getName() + " does not support taking screenshots.");
         }
+    }
+
+    /**
+     * The root path for the screenshots.
+     * Defaults to "target/jbehave/screenshots"
+     * @return the root path.
+     */
+    public String getScreenShotRootPath() {
+        return "target/jbehave/screenshots";
     }
 
 }
