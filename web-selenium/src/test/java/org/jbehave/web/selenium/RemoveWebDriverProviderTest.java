5ad531983005e737b0ce2f99457cefa388c096c3
Mauro Talevi <mauro.talevi@aquilonia.org>
2011-04-14 19:18:03 +0200

    JBEHAVE-482:  Added unit test to verify behaviour for @BeforeStory failures.

--- web-selenium/src/test/java/org/jbehave/web/selenium/RemoveWebDriverProviderTest.java
+++ web-selenium/src/test/java/org/jbehave/web/selenium/RemoveWebDriverProviderTest.java
@@ -12,13 +12,23 @@ import org.jbehave.core.embedder.Embedder.RunningStoriesFailed;
 import org.jbehave.core.embedder.EmbedderControls;
 import org.jbehave.core.io.StoryLoader;
 import org.jbehave.core.steps.InstanceStepsFactory;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class RemoveWebDriverProviderTest {
 
     @Test(expected = RunningStoriesFailed.class)
-    public void shouldFailUponInitializeDueToMissingURL() throws Throwable {
-        MyPerStoriesWebDriverSteps steps = new MyPerStoriesWebDriverSteps(new RemoteWebDriverProvider());
+    @Ignore("FIXME")
+    public void shouldFailUponInitialiseWhenRunningWithPerStoriesWebDriverSteps() throws Throwable {
+        runStory(new MyPerStoriesWebDriverSteps(new RemoteWebDriverProvider()));
+    }
+
+    @Test(expected = RunningStoriesFailed.class)
+    public void shouldFailUponInitialiseWhenRunningWithPerStoryWebDriverSteps() throws Throwable {
+        runStory(new MyPerStoryWebDriverSteps(new RemoteWebDriverProvider()));
+    }
+
+    private void runStory(WebDriverSteps steps) {
         final String story = "Scenario: A simple web scenario\n" 
                 + "Given a test\n"
                 + "When a test is executed\n"
@@ -44,6 +54,31 @@ public class RemoveWebDriverProviderTest {
     public class MyPerStoriesWebDriverSteps extends PerStoriesWebDriverSteps {
 
         public MyPerStoriesWebDriverSteps(WebDriverProvider driverProvider) {
+            super(driverProvider);
+        }
+
+        @Given("a test")
+        public void aTest() {
+        }
+
+        @When("a test is executed")
+        public void aTestIsExecuted() {
+        }
+
+        @When("a test fails")
+        public void aTestFails() {
+            throw new RuntimeException("Test failed");
+        }
+
+        @Then("a tester is a happy hopper")
+        public void aTesterIsHappy() {
+        }
+    };
+
+    
+    public class MyPerStoryWebDriverSteps extends PerStoryWebDriverSteps {
+
+        public MyPerStoryWebDriverSteps(WebDriverProvider driverProvider) {
             super(driverProvider);
         }
 
