2998f6b0dfbc35380597f3a41f6ac5a509581606
Mauro Talevi <mauro.talevi@aquilonia.org>
2011-05-18 19:57:21 +0100

    Updated TraderWebStories to use steps factory configuration.

--- examples/trader-runner-selenium/src/main/java/org/jbehave/web/examples/trader/selenium/TraderWebStories.java
+++ examples/trader-runner-selenium/src/main/java/org/jbehave/web/examples/trader/selenium/TraderWebStories.java
@@ -1,36 +1,33 @@
 package org.jbehave.web.examples.trader.selenium;
 
-import static java.util.Arrays.asList;
-import static org.jbehave.core.io.CodeLocations.codeLocationFromClass;
-import static org.jbehave.core.reporters.StoryReporterBuilder.Format.CONSOLE;
-import static org.jbehave.core.reporters.StoryReporterBuilder.Format.HTML;
-import static org.jbehave.core.reporters.StoryReporterBuilder.Format.TXT;
-import static org.jbehave.core.reporters.StoryReporterBuilder.Format.XML;
-
 import java.util.List;
 
 import org.jbehave.core.Embeddable;
 import org.jbehave.core.configuration.Configuration;
-import org.jbehave.core.io.CodeLocations;
 import org.jbehave.core.io.LoadFromClasspath;
 import org.jbehave.core.io.StoryFinder;
 import org.jbehave.core.junit.JUnitStories;
-import org.jbehave.core.reporters.ConsoleOutput;
-import org.jbehave.core.reporters.StoryReporter;
 import org.jbehave.core.reporters.StoryReporterBuilder;
-import org.jbehave.core.steps.CandidateSteps;
+import org.jbehave.core.steps.InjectableStepsFactory;
 import org.jbehave.core.steps.InstanceStepsFactory;
 import org.jbehave.core.steps.SilentStepMonitor;
 import org.jbehave.web.examples.trader.selenium.pages.Pages;
 import org.jbehave.web.examples.trader.selenium.steps.TraderWebSteps;
-import org.jbehave.web.selenium.SeleniumScreenshotOnFailure;
 import org.jbehave.web.selenium.PerStoriesSeleniumSteps;
 import org.jbehave.web.selenium.SeleniumConfiguration;
 import org.jbehave.web.selenium.SeleniumContext;
+import org.jbehave.web.selenium.SeleniumScreenshotOnFailure;
 import org.jbehave.web.selenium.SeleniumStepMonitor;
 
 import com.thoughtworks.selenium.Selenium;
 import com.thoughtworks.selenium.condition.ConditionRunner;
+
+import static java.util.Arrays.asList;
+import static org.jbehave.core.io.CodeLocations.codeLocationFromClass;
+import static org.jbehave.core.reporters.Format.CONSOLE;
+import static org.jbehave.core.reporters.Format.HTML;
+import static org.jbehave.core.reporters.Format.TXT;
+import static org.jbehave.core.reporters.Format.XML;
 
 public class TraderWebStories extends JUnitStories {
 
@@ -47,39 +44,20 @@ public class TraderWebStories extends JUnitStories {
             .useSeleniumContext(seleniumContext)
             .useStepMonitor(new SeleniumStepMonitor(selenium, seleniumContext, new SilentStepMonitor()))
             .useStoryLoader(new LoadFromClasspath(embeddableClass))
-            .useStoryReporterBuilder(new StoryReporterBuilder(){
-
-                    @Override
-                    public StoryReporter reporterFor(String storyPath, Format format) {
-                        if ( format == CONSOLE ){
-                            return new ConsoleOutput(){
-                                @Override
-                                public void beforeScenario(String title) {
-                                    seleniumContext.setCurrentScenario(title);
-                                    super.beforeScenario(title);
-                                }                                
-                            };
-                        } else { 
-                            return super.reporterFor(storyPath, format);
-                        }
-                    }
-                
-                }
-                .withCodeLocation(CodeLocations.codeLocationFromClass(embeddableClass))
+            .useStoryReporterBuilder(new StoryReporterBuilder()
+                .withCodeLocation(codeLocationFromClass(embeddableClass))
                 .withDefaultFormats()
                 .withFormats(CONSOLE, TXT, HTML, XML));
     }
 
     @Override
-    public List<CandidateSteps> candidateSteps() {
+    public InjectableStepsFactory stepsFactory() {
         return new InstanceStepsFactory(configuration(),
                 new TraderWebSteps(pages),
                 new PerStoriesSeleniumSteps(selenium),
-                new SeleniumScreenshotOnFailure(selenium))
-                .createCandidateSteps();
+                new SeleniumScreenshotOnFailure(selenium));
     }
     
-
     @Override
     protected List<String> storyPaths() {
         return new StoryFinder()
--- examples/trader-runner-webdriver/src/main/java/org/jbehave/web/examples/trader/webdriver/TraderWebStories.java
+++ examples/trader-runner-webdriver/src/main/java/org/jbehave/web/examples/trader/webdriver/TraderWebStories.java
@@ -1,37 +1,34 @@
 package org.jbehave.web.examples.trader.webdriver;
 
-import static java.util.Arrays.asList;
-import static org.jbehave.core.io.CodeLocations.codeLocationFromClass;
-import static org.jbehave.core.reporters.StoryReporterBuilder.Format.HTML;
-import static org.jbehave.core.reporters.StoryReporterBuilder.Format.IDE_CONSOLE;
-import static org.jbehave.core.reporters.StoryReporterBuilder.Format.TXT;
-import static org.jbehave.core.reporters.StoryReporterBuilder.Format.XML;
-
 import java.util.List;
 
 import org.jbehave.core.Embeddable;
 import org.jbehave.core.configuration.Configuration;
-import org.jbehave.core.io.CodeLocations;
 import org.jbehave.core.io.LoadFromClasspath;
 import org.jbehave.core.io.StoryFinder;
 import org.jbehave.core.junit.JUnitStories;
-import org.jbehave.core.reporters.ConsoleOutput;
-import org.jbehave.core.reporters.StoryReporter;
 import org.jbehave.core.reporters.StoryReporterBuilder;
-import org.jbehave.core.steps.CandidateSteps;
+import org.jbehave.core.steps.InjectableStepsFactory;
 import org.jbehave.core.steps.InstanceStepsFactory;
 import org.jbehave.core.steps.SilentStepMonitor;
 import org.jbehave.web.examples.trader.webdriver.pages.Pages;
-import org.jbehave.web.selenium.WebDriverScreenshotOnFailure;
 import org.jbehave.web.examples.trader.webdriver.steps.TraderWebSteps;
 import org.jbehave.web.selenium.ContextView;
-import org.jbehave.web.selenium.PerStoriesWebDriverSteps;
 import org.jbehave.web.selenium.LocalFrameContextView;
+import org.jbehave.web.selenium.PerStoriesWebDriverSteps;
 import org.jbehave.web.selenium.PropertyWebDriverProvider;
 import org.jbehave.web.selenium.SeleniumConfiguration;
 import org.jbehave.web.selenium.SeleniumContext;
 import org.jbehave.web.selenium.SeleniumStepMonitor;
 import org.jbehave.web.selenium.WebDriverProvider;
+import org.jbehave.web.selenium.WebDriverScreenshotOnFailure;
+
+import static java.util.Arrays.asList;
+import static org.jbehave.core.io.CodeLocations.codeLocationFromClass;
+import static org.jbehave.core.reporters.Format.CONSOLE;
+import static org.jbehave.core.reporters.Format.HTML;
+import static org.jbehave.core.reporters.Format.TXT;
+import static org.jbehave.core.reporters.Format.XML;
 
 public class TraderWebStories extends JUnitStories {
 
@@ -48,41 +45,19 @@ public class TraderWebStories extends JUnitStories {
                 .useWebDriverProvider(driverProvider)
                 .useStepMonitor(new SeleniumStepMonitor(contextView, context, new SilentStepMonitor()))
                 .useStoryLoader(new LoadFromClasspath(embeddableClass))
-                .useStoryReporterBuilder(new StoryReporterBuilder() {
-                            @Override
-                            public StoryReporter reporterFor(String storyPath, Format format) {
-                                if (format == IDE_CONSOLE) {
-                                    return new ConsoleOutput() {
-                                        @Override
-                                        public void beforeScenario(String scenarioTitle) {
-                                            context.setCurrentScenario(scenarioTitle);
-                                            super.beforeScenario(scenarioTitle);
-                                        }
-
-                                        @Override
-                                        public void afterStory(boolean givenStory) {
-                                            contextView.close();
-                                            super.afterStory(givenStory);
-                                        }
-                                    };
-                                } else {
-                                    return super.reporterFor(storyPath, format);
-                                }
-                            }
-
-                        }
-                    .withCodeLocation(CodeLocations.codeLocationFromClass(embeddableClass))
+                .useStoryReporterBuilder(new StoryReporterBuilder()
+                    .withCodeLocation(codeLocationFromClass(embeddableClass))
                     .withDefaultFormats()
-                    .withFormats(IDE_CONSOLE, TXT, HTML, XML));
+                    .withFormats(CONSOLE, TXT, HTML, XML));
     }
 
     @Override
-    public List<CandidateSteps> candidateSteps() {
+    public InjectableStepsFactory stepsFactory() {
         Configuration configuration = configuration();
         return new InstanceStepsFactory(configuration, 
                 new TraderWebSteps(pages),
                 new PerStoriesWebDriverSteps(driverProvider), // or PerStoryWebDriverSteps or PerScenarioWebDriverSteps
-                new WebDriverScreenshotOnFailure(driverProvider, configuration.storyReporterBuilder())).createCandidateSteps();
+                new WebDriverScreenshotOnFailure(driverProvider, configuration.storyReporterBuilder()));
     }
 
 
