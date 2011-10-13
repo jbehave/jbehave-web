bdfb35e9ef6f4edf27523d1dfb48bdc9e79571d0
Paul Hammant <paul@hammant.org>
2011-10-13 07:09:37 -0500

    JBEHAVE-613: hybrid inection type for picocontainer and groovy + WebDriver

---
+++ web-selenium/src/main/java/org/jbehave/web/selenium/WebdriverConstructorAndSetterInjection.java
@@ -0,0 +1,42 @@
+package org.jbehave.web.selenium;
+
+import groovy.lang.MetaClass;
+import org.openqa.selenium.WebElement;
+import org.picocontainer.ComponentAdapter;
+import org.picocontainer.ComponentMonitor;
+import org.picocontainer.LifecycleStrategy;
+import org.picocontainer.Parameter;
+import org.picocontainer.PicoCompositionException;
+import org.picocontainer.injectors.CompositeInjection;
+import org.picocontainer.injectors.ConstructorInjection;
+import org.picocontainer.injectors.SetterInjection;
+import org.picocontainer.injectors.SetterInjector;
+
+import java.lang.reflect.Method;
+import java.util.Properties;
+
+public class WebdriverConstructorAndSetterInjection extends CompositeInjection {
+    public WebdriverConstructorAndSetterInjection() {
+        super(new ConstructorInjection(),
+                new SetterInjection() {
+                    @Override
+                    public <T> ComponentAdapter<T> createComponentAdapter(ComponentMonitor monitor, LifecycleStrategy lifecycleStrategy, Properties componentProperties, Object componentKey, Class<T> componentImplementation, Parameter... parameters) throws PicoCompositionException {
+                        return new SetterInjector<T>(componentKey, componentImplementation, parameters, monitor, "set", "", false, true) {
+                            @Override
+                            protected boolean isInjectorMethod(Method method) {
+                                Class<?> pType = method.getParameterTypes()[0];
+                                // WebElements as properties, are not injected by PicoContainer.
+                                // See http://code.google.com/p/selenium/wiki/PageFactory
+                                // Similarly, there are some inner-class properties made by Groovy's
+                                // compiler that should not be injected.
+                                if (pType.equals(WebElement.class) || pType.getName().matches("^.*Component$")) {
+                                    return false;
+                                }
+                                // Groovy classes have a setMetaClass(MetaClass mc) method which we don't want to inject into.
+                                return pType != MetaClass.class && super.isInjectorMethod(method);
+                            }
+                        };
+                    }
+                });
+    }
+}
---
+++ web-selenium/src/test/java/org/jbehave/web/selenium/WebdriverConstructorAndSetterInjectionTest.java
@@ -0,0 +1,108 @@
+package org.jbehave.web.selenium;
+
+import groovy.lang.MetaClass;
+import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebElement;
+import org.picocontainer.DefaultPicoContainer;
+import org.picocontainer.MutablePicoContainer;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.mock;
+
+public class WebdriverConstructorAndSetterInjectionTest {
+
+    private WebdriverConstructorAndSetterInjection injection = new WebdriverConstructorAndSetterInjection();
+    private DefaultPicoContainer mpc = new DefaultPicoContainer(injection);
+
+    @Before
+    public void setUp() throws Exception {
+        mpc.addComponent("Hello");
+        mpc.addComponent(mock(WebElement.class));
+        mpc.addComponent(mock(MetaClass.class));
+    }
+
+    @Test
+    public void constructorInjectionShouldBePossible() {
+        WillDoThis result = mpc.addComponent(WillDoThis.class).getComponent(WillDoThis.class);
+        assertThat(result.field, equalTo("Hello"));
+    }
+
+    @Test
+    public void setterInjectionShouldBePossible() {
+        WillDoThis2 result = mpc.addComponent(WillDoThis2.class).getComponent(WillDoThis2.class);
+        assertThat(result.field, equalTo("Hello"));
+    }
+
+    @Test
+    public void fieldInjectionShouldNotBeAllowed() {
+        WontDoThis result = mpc.addComponent(WontDoThis.class).getComponent(WontDoThis.class);
+        assertThat(result.field, nullValue());
+    }
+
+    @Test
+    public void webElementShouldNotBeAllowed() {
+        WontDoThis2 result = mpc.addComponent(WontDoThis2.class).getComponent(WontDoThis2.class);
+        assertThat(result.field, nullValue());
+    }
+
+    @Test
+    public void metaClassShouldNotBeAllowed() {
+        WontDoThis3 result = mpc.addComponent(WontDoThis3.class).getComponent(WontDoThis3.class);
+        assertThat(result.field, nullValue());
+    }
+
+    @Test
+    public void groovyInnerClassPropertiesShouldNotBeAllowed() {
+        WontDoThis4 result = mpc.addComponent(WontDoThis4.class)
+                .addComponent(WontDoThis4Component.class)
+                .getComponent(WontDoThis4.class);
+        assertThat(result.field, nullValue());
+    }
+
+    public static class WontDoThis {
+        public String field;
+    }
+
+    public static class WillDoThis {
+        private String field;
+        public WillDoThis(String field) {
+            this.field = field;
+        }
+    }
+
+    public static class WillDoThis2 {
+        private String field;
+        public void setField(String field) {
+            this.field = field;
+        }
+    }
+
+    public static class WontDoThis2 {
+        private WebElement field;
+        public void setField(WebElement field) {
+            this.field = field;
+        }
+    }
+
+    public static class WontDoThis3 {
+        private MetaClass field;
+        public void setField(MetaClass field) {
+            this.field = field;
+        }
+    }
+
+    public static class WontDoThis4 {
+        private WontDoThis4Component field;
+        public void setField(WontDoThis4Component field) {
+            this.field = field;
+        }
+    }
+    public static class WontDoThis4Component {
+    }
+
+
+
+}
