ad392c3282da2922571585de970fc47af41fb643
Mauro Talevi <mauro.talevi@aquilonia.org>
2012-04-28 19:08:24 +0200

    JBEHAVE-772:  Added archetype properties for jbehave versions.

--- archetypes/update-from-tutorials.sh
+++ archetypes/update-from-tutorials.sh
@@ -5,9 +5,14 @@ for NAME in "groovy-pico" "java-spring"
 do
 TUTORIAL="$TUTORIALS/$NAME"
 cd $TUTORIAL
-mvn archetype:create-from-project
+mvn clean archetype:create-from-project
 ARCHETYPE="$ARCHETYPES/web-selenium-$NAME-archetype/"
-cp -pr $TUTORIAL/target/generated-sources/archetype/src $ARCHETYPE
+GENERATED="$TUTORIAL/target/generated-sources/archetype"
+perl -p -i -e 's/<fileSets>/<requiredProperties>\n<requiredProperty key="jbehaveCoreVersion">\n<defaultValue>\${jbehave.core.version}<\/defaultValue>\n<\/requiredProperty>\n<requiredProperty key="jbehaveWebVersion">\n<defaultValue>\${jbehave.web.version}<\/defaultValue>\n<\/requiredProperty>\n<requiredProperty key="jbehaveSiteVersion">\n<defaultValue>\${jbehave.site.version}<\/defaultValue>\n<\/requiredProperty>\n<\/requiredProperties>\n<fileSets>/g' $GENERATED/src/main/resources/META-INF/maven/archetype-metadata.xml 
+perl -p -i -e 's/<jbehave.core.version>(.*)<\/jbehave.core.version>/<jbehave.core.version>\$jbehaveCoreVersion<\/jbehave.core.version>/g' $GENERATED/src/main/resources/archetype-resources/pom.xml
+perl -p -i -e 's/<jbehave.web.version>(.*)<\/jbehave.web.version>/<jbehave.web.version>\$jbehaveWebVersion<\/jbehave.web.version>/g' $GENERATED/src/main/resources/archetype-resources/pom.xml 
+perl -p -i -e 's/<jbehave.site.version>(.*)<\/jbehave.site.version>/<jbehave.site.version>\$jbehaveSiteVersion<\/jbehave.site.version>/g' $GENERATED/src/main/resources/archetype-resources/pom.xml 
+cp -pr $GENERATED/src $ARCHETYPE
 done 
 
 cd $ARCHETYPES
